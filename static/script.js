36d0ab4312e772fb9940c62984d6d32334d1d170
DietrichSabotka06 <uniqueblue274@gmail.com>
2026-04-05 00:58:32 -0400

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,41 @@
+async function loadRecipes() {
+  const res = await fetch('/api/recipes');
+  const data = await res.json();
+
+  const grid = document.getElementById('recipe-grid');
+  grid.innerHTML = "";
+
+  data.forEach(r => {
+    const div = document.createElement('div');
+    div.className = "card";
+
+    div.innerHTML = `
+      <a href="${r.link}" target="_blank">
+        <img src="${r.image}">
+      </a>
+      <h3>${r.title}</h3>
+      <a href="/recipes/${r.file}" target="_blank">Open Recipe File</a>
+    `;
+
+    grid.appendChild(div);
+  });
+}
+
+async function addRecipe() {
+  const data = {
+    title: document.getElementById("title").value,
+    image: document.getElementById("image").value,
+    link: document.getElementById("link").value,
+    file: document.getElementById("file").value
+  };
+
+  await fetch('/api/add', {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(data)
+  });
+
+  loadRecipes();
+}
+
+loadRecipes();
